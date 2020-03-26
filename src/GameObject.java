8dde26e2051431f8cd68afd9dcb6314c83ed9f2c
CCheng17783 <c.cheng17783@gmail.com>
2020-03-26 16:03:46 +0800

    20200326_add GameObject

---
+++ src/GameObject.java
@@ -0,0 +1,21 @@
+import java.awt.image.BufferedImage;
+
+public abstract class GameObject {
+	protected BufferedImage img;
+	protected int x;
+	protected int y;
+	protected int width;
+	protected int height;
+	protected int left;
+	protected int right;
+	protected int top;
+	protected int bottom;
+	
+	public GameObject(String imgPath,int width, int height) {
+		this.width = width;
+		this.height = height;
+		
+	}
+	
+
+}
